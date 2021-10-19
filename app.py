cccda616b1c28073b1461591c67424bdfb46c27d
HemantKhuspe <75375911+HemantKhuspe@users.noreply.github.com>
2021-10-19 23:23:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ def predict():
             N=float(request.form['N'])
             P=float(request.form['P'])
             K=float(request.form['K'])
+            temperature=float(request.form['temperature'])
+            humidity=float(request.form['humidity'])
             ph=float(request.form['ph'])
             rainfall=float(request.form['rainfall'])
             pred_args= [N,P,K,ph,rainfall]
